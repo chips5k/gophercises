b1f45baf4b660afe6236b1b0cdb1c0aba564f26c
chips5k <callem.pittard@myob.com>
2020-11-05 09:48:34 +1100

    load the file

--- link/main.go
+++ link/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
 
 func main() {
-	fmt.Println("test")
+	path := flag.String("path", "", "Path to html file")
+	flag.Parse()
+
+	if *path == "" {
+		flag.Usage()
+	}
+
+	bb, err := ioutil.ReadFile(*path)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading %s: %v", *path, err)
+		os.Exit(1)
+	}
+
+	fmt.Println(string(bb))
 }
